457584ec73bee0dae4e3d9de79b1cf1ed6a9e165
Amination1 <seyyed.amin.8809@gmail.com>
2025-02-01 22:43:25 +0330

    git init

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/cookie', function () {
+    return [$_COOKIE];
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
