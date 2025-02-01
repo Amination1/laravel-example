f28040934dbf72852299c4558344e4defd2d2fe5
Amination1 <seyyed.amin.8809@gmail.com>
2025-02-02 00:16:37 +0330

    Edit images from layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
     <h1>Contact page 𝌕</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
     <h1>Home page 🏠🏡</h1>
 </x-layout>
